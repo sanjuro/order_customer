8c2b6ec1b8a8153c8e67deb52cbcfbc9500a1ebd
Flippie Scholtz <flippiescholtz@gmail.com>
2013-03-20 18:01:26 +0200

    Added permissions and libraries for gcm.

---
+++ src/com/vosto/customer/GCMIntentService.java
@@ -0,0 +1,37 @@
+package com.vosto.customer;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class GCMIntentService extends com.google.android.gcm.GCMBaseIntentService {
+
+	public GCMIntentService(){
+		super("1091536520954");
+	}
+	
+	@Override
+	protected void onError(Context arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onMessage(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onRegistered(Context arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onUnregistered(Context arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
--- src/com/vosto/customer/accounts/activities/SignUpActivity.java
+++ src/com/vosto/customer/accounts/activities/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.vosto.customer.accounts.activities;
 
+import com.google.android.gcm.GCMRegistrar;
 import com.vosto.customer.HomeActivity;
 import com.vosto.customer.R;
 import com.vosto.customer.accounts.services.CreateAccountResult;
@@ -14,18 +15,30 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class SignUpActivity extends Activity implements OnRestReturn {
 	
 	private ProgressDialog pleaseWaitDialog;
+	private String gcmRegistrationId; // The id assigned by gcm for this app / device combination
 	
 	@Override
     protected void onCreate(Bundle args)
     {
         super.onCreate(args);
-        setContentView(R.layout.activity_signup);   
+        setContentView(R.layout.activity_signup);  
+        
+        // Check if GCM is OK and see if there's an existing registration id:
+        try{
+        	GCMRegistrar.checkDevice(this);
+        	GCMRegistrar.checkManifest(this);
+        	 this.gcmRegistrationId = GCMRegistrar.getRegistrationId(this);
+        }catch(Exception e){
+        	// GCM is not supported or permissions not set up correctly.
+        	Log.d("GCM", "GCM can't be initialized. GCM not supported or problem with manifest.");
+        }
     }
 	
 	public void signInClicked(View v){
@@ -97,6 +110,16 @@ public class SignUpActivity extends Activity implements OnRestReturn {
 				   editor.putString("userToken", createResult.getAccountResponseWrapper().authentication_token);
 				   editor.putString("userName", createResult.getAccountResponseWrapper().first_name);
 				   editor.commit();
+				   
+				   /*
+				    *  Register the device with GCM if not already registered.
+				    *  GCM will respond and the callback in GCMIntentService will be called.
+				    */
+				   if(this.gcmRegistrationId != null && this.gcmRegistrationId.equals("")){
+					   // GCM is supported, but device has not been registered yet.
+					   GCMRegistrar.register(this, "1091536520954");
+				   }
+				   
 				Intent intent = new Intent(this, HomeActivity.class);
 		    	startActivity(intent);
 		    	finish();
